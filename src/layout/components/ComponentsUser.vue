7e91af77234018e508bc3ca1a89a96b0237320db
fexiao <fexiao@tencent.com>
2024-03-14 18:19:34 +0800

    fix: 头部样式编写

---
+++ src/layout/components/ComponentsUser.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="flex">
+      <div class="user_logo"></div>
+      <div class="user_name flex_1">
+        <div class="f_w text_overflow">{{ userInfo.name || "-" }}</div>
+        <div class="user_id">ID:{{ userInfo.id || "-" }}</div>
+      </div>
+    </div>
+    <div class="user_button">开通会员</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userInfo: {
+        name: "贴图",
+        id: "14523654",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.user_logo {
+  width: 40px;
+  height: 40px;
+  border-radius: 28px 28px 28px 28px;
+  background: #333649;
+  margin-right: 15px;
+}
+
+.user_name {
+  width: calc(100% - 55px);
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.user_id {
+  font-size: 12px;
+  color: #989ba4;
+}
+
+.user_button {
+  width: 100%;
+  height: 32px;
+  line-height: 32px;
+  text-align: center;
+  background: #ed702d;
+  border-radius: 3px 3px 3px 3px;
+}
+</style>
--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -1,11 +1,55 @@
 <template>
   <div class="header">
-    <div class="main-box">111111</div>
+    <div class="main-box flex flex_between">
+      <div class="header_left">贴图网</div>
+      <div class="flex">
+        <div class="search_box">
+          <span>请输入你想搜索的内容</span>
+          <img src="assets/cut.png" alt="" class="cut_icon" />
+          <img src="assets/search.png" alt="" class="search_icon" />
+        </div>
+        <el-popover placement="bottom" :width="180" trigger="click">
+          <template #reference>
+            <div class="header_user"></div>
+          </template>
+          <ComponentsUser />
+        </el-popover>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import ComponentsUser from "./ComponentsUser.vue";
+export default {
+  components: { ComponentsUser },
+  data() {
+    return {
+      gridData: [
+        {
+          date: "2016-05-02",
+          name: "Jack",
+          address: "New York City",
+        },
+        {
+          date: "2016-05-04",
+          name: "Jack",
+          address: "New York City",
+        },
+        {
+          date: "2016-05-01",
+          name: "Jack",
+          address: "New York City",
+        },
+        {
+          date: "2016-05-03",
+          name: "Jack",
+          address: "New York City",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="less" scoped>
@@ -15,5 +59,40 @@ export default {};
   background: #ffffff;
   box-shadow: 0px 5px 20px 1px rgba(51, 54, 73, 0.15);
   border-radius: 3px 3px 3px 3px;
+
+  .search_box {
+    width: 580px;
+    height: 32px;
+    background: #ffffff;
+    border-radius: 3px 3px 3px 3px;
+    border: 1px solid #d6d7db;
+    position: relative;
+    line-height: 32px;
+    padding-left: 12px;
+    color: #989ba4;
+    cursor: pointer;
+
+    img {
+      width: 16px;
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+
+    .search_icon {
+      right: 16px;
+    }
+    .cut_icon {
+      right: 52px;
+    }
+  }
+
+  .header_user {
+    width: 32px;
+    height: 32px;
+    background: #333649;
+    border-radius: 20px 20px 20px 20px;
+    margin-left: 26px;
+  }
 }
 </style>
