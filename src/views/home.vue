07a4442b19c6d828a3bbd65214078fb49dbbd41b
xfking <643314606@qq.com>
2024-03-24 14:18:50 +0800

    fix: 主体内容编写

--- src/views/home.vue
+++ src/views/home.vue
@@ -13,7 +13,7 @@
     </div>
     <!-- 排序 -->
     <div class="score flex">
-      <div>排序</div>
+      <div class="score_item">排序</div>
       <div
         v-for="(item, index) in scoreList"
         :key="index"
@@ -23,6 +23,40 @@
       >
         {{ item.name }}
       </div>
+    </div>
+
+    <!-- 贴图主体 -->
+    <div class="content flex">
+      <!-- 骨架屏 -->
+      <el-space :size="10" wrap alignment="flex-start">
+        <el-skeleton
+          :loading="loading"
+          animated
+          :throttle="200"
+          v-for="(item, index) in 8"
+          :key="index"
+        >
+          <template #template>
+            <div class="card">
+              <el-skeleton-item variant="image" class="image" />
+              <div style="padding: 14px">
+                <el-skeleton-item variant="h3" style="width: 100%" />
+              </div>
+            </div>
+          </template>
+          <template #default>
+            <el-card shadow="hover" :body-style="{ padding: '5px' }">
+              <img
+                src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+                class="image"
+              />
+              <div style="padding: 14px; width: 220px" class="text_overflow">
+                无缝浅橡饰面板材木纹无缝浅橡饰面板材木纹无缝浅橡饰面板材木纹
+              </div>
+            </el-card>
+          </template>
+        </el-skeleton>
+      </el-space>
     </div>
   </div>
 </template>
@@ -60,11 +94,30 @@ export default {
           name: "最新",
         },
       ],
+      loading: true,
+      list: [],
     };
   },
+  created() {
+    // 初始化
+    this.handLoading();
+  },
   methods: {
+    // 选择排序
     handSelect(index) {
       this.score = index;
+      this.handLoading();
+    },
+
+    // 加载动画
+    handLoading() {
+      this.loading = true;
+      const timer = setTimeout(() => {
+        if (timer) {
+          clearTimeout(timer);
+        }
+        this.loading = false;
+      }, 2000);
     },
   },
 };
@@ -82,15 +135,42 @@ export default {
   border-radius: 3px 3px 3px 3px;
 }
 
+.score {
+  width: 100%;
+  padding: 14px 36px;
+  margin: 30px 0;
+  background: #ffffff;
+  box-shadow: 0px 5px 20px 1px rgba(51, 54, 73, 0.15);
+  border-radius: 6px 6px 6px 6px;
+  box-sizing: border-box;
+}
+
 .score_item {
   padding: 4px 12px;
+  margin-right: 24px;
   background: #fff;
   border-radius: 4px 4px 4px 4px;
   cursor: pointer;
+  font-size: 16px;
 }
 
 .score_select {
   color: #fff;
   background: #ed702d;
+}
+
+.content {
+  width: 100%;
+
+  .image {
+    width: 220px;
+    height: 220px;
+    border-radius: 8px;
+  }
+
+  .card {
+    border: none;
+    padding: 6px !important;
+  }
 }
 </style>
